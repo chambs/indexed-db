fb67a88e11864d0904f95e05221c8d006564e13a
Willian Carvalho <o.chambs@gmail.com>
2014-04-10 07:17:55 -0300

    Added on ugrade needed. not so many commits so far because I an reading about the whole thing

--- app.js
+++ app.js
@@ -2,18 +2,38 @@ if(!window.indexedDB) {
 	window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
 }
 
+var db;
+
 function onSucess(ev) {
 	console.log('success');
+  db = this.result;
+
+  db.onerror = function(ev) {
+    alert('database error: ' + ev.target.errorCode);
+  };
 }
 
 function onError(ev) {
-	console.log('error');
+  console.log('error');
+}
+
+function onUpgradeNeeded(ev) {
+  console.log('update fired');
+  var db = ev.target.result,
+      objectStore = db.createObjectStore('name', {
+        keyPath: 'myKey'
+      })
+  ;
+
 }
 
 
 function app() {
-	var request = indexedDB.open('mydb');
+	var request = indexedDB.open('mydb', 2);
 
 	request.onsuccess = onSucess;
 	request.onerror = onError;
+  request.onupgradeneeded = onUpgradeNeeded;
 }
+
+app();
