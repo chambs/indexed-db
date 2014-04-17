de8d6b98d71fa2fb1cd12b84b4b4fbb1401eedec
Willian Carvalho <o.chambs@gmail.com>
2014-04-16 23:23:02 -0300

    I am finally startting to build some kind of IndexedDB wrapper. It is called zonDB and it can be instantiated with var myDb = new zonDB('dbname', dbversion); It has the following methods: - open: opens the database - addTable: adds object store objects to the db - removeTable: removes object store objects from the db

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-if(!window.indexedDB) {
-	window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
-}
-
 var db;
 
 function onSucess(ev) {
@@ -9,7 +5,25 @@ function onSucess(ev) {
   db = this.result;
 
   db.onerror = function(ev) {
-    alert('database error: ' + ev.target.errorCode);
+    console.log(ev);
+    alert('database error: ' + ev.target.error.message);
+  };
+
+  var transaction = db.transaction('name', 'readwrite');
+  var objectStore = transaction.objectStore('name');
+
+  var req = objectStore.get('o.chambs@gmail.com');
+  req.onsuccess = function(event) {
+    console.log(req, event, 'SUCCESS');
+  };
+
+  //retrieve all rows
+  objectStore.openCursor().onsuccess = function(event) {
+    var cursor = event.target.result;
+    if(cursor) {
+      console.log(cursor.key, cursor.value, '<<<');
+      cursor.continue();
+    }
   };
 }
 
@@ -22,11 +36,9 @@ function onUpgradeNeeded(ev) {
   var db = ev.target.result,
       objectStore = db.createObjectStore('name', {
         keyPath: 'myKey'
-      })
+      });
   ;
-
 }
-
 
 function app() {
 	var request = indexedDB.open('mydb', 2);
@@ -36,4 +48,75 @@ function app() {
   request.onupgradeneeded = onUpgradeNeeded;
 }
 
-app();
+(function(window) {
+  if(!window.indexedDB) {
+    window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+  }
+
+  function zonDB(dbName, dbVersion) {
+    this.name = dbName;
+    this.version = dbVersion;
+    this.request = null;
+    this.dbReady = false;
+    this.tablesToBeAdded = [];
+    this.tablesToBeRemoved = [];
+  }
+
+  zonDB.prototype = {
+    open: function() {
+      var that = this;
+      this.request = indexedDB.open(this.name, this.version);
+
+      this.request.onsuccess = function(ev) {
+        that.dbReady = true;
+        console.log(ev.target, 'SUCCESS');
+      };
+
+      this.request.onerror = function(ev) {
+        console.log(ev.target, 'ERROR');
+      };
+
+      this.request.onupgradeneeded = function(ev) {
+        var db = ev.target.result,
+            tbData, tbName;
+        window.foo = db;
+
+        that.tablesToBeAdded.forEach(function(el) {
+          tbName = el.tableName;
+          delete el.name;
+          db.createObjectStore(tbName, el);
+        });
+
+        that.tablesToBeRemoved.forEach(function(el) {
+          db.deleteObjectStore(el);
+        });
+
+        that.tablesToBeAdded.length = 0;
+        console.log('UPGRADE DONE');
+      };
+    },
+
+    addTable: function(options) {
+      this.tablesToBeAdded.push(options);
+    },
+
+    removeTable: function(tbName) {
+      this.tablesToBeRemoved.push(tbName);
+    }
+  };
+
+  window.zonDB = zonDB;
+
+})(this);
+
+var todo = new zonDB('todo', 4);
+
+todo.removeTable('undefined');
+
+/*
+todo.addTable({
+  tableName: 'user',
+  keyPath: 'id'
+});
+*/
+todo.open();
