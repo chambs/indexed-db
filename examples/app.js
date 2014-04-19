76fb8699dc42279eb844d758dfae88c3ad87f08f
Willian Carvalho <o.chambs@gmail.com>
2014-04-19 13:00:30 -0300

    Implementing update on the example

--- examples/app.js
+++ examples/app.js
@@ -8,6 +8,11 @@ todo.addTable({
 });
 
 function addUser() {
+  if($('#id').val()) {
+    updateUser();
+    return;
+  }
+
   var name = $('#name').val(),
       age = $('#age').val(),
       userData = {
@@ -17,11 +22,72 @@ function addUser() {
 
   todo.getTable('user', function(tb) {
     tb.add(userData);
+    $('#name').val('');
+    $('#age').val('');
   });
 }
 
-$(document).on('click', '.add-user', function() {
+function removeUser(id) {
+  todo.getTable('user', function(tb) {
+    tb.delete(id);
+    listUsers();
+  });
+}
+
+function editUser(id) {
+  var userData;
+  todo.getTable('user', function(tb) {
+    tb.get(id).onsuccess = function(ev) {
+      userData = ev.target.result;
+      $('#name').val(userData.name);
+      $('#age').val(userData.age);
+      $('#id').val(userData.id);
+    };
+  });
+}
+
+function updateUser() {
+  var newData = {
+    id: +$('#id').val(),
+    name: $('#name').val(),
+    age: $('#age').val()
+  };
+
+  todo.getTable('user', function(tb) {
+    var req = tb.put(newData);
+
+    $('#name').val('');
+    $('#age').val('');
+    $('#id').val('');
+  });
+}
+
+function listUsers() {
+  table.empty();
+  todo.query('user', function(res) {
+    res.forEach(function(row) {
+      table.append("<tr>");
+      table.append("<td>" + row.id + "</td>");
+      table.append("<td>" + row.name + "</td>");
+      table.append("<td>" + row.age + "</td>");
+      table.append("<td><a href='#' class='edit-row' data-id='"+row.id+"'>Edit</a> / <a href='#' class='remove-row' data-id='"+row.id+"'>Remove</a></td>");
+      table.append("</tr>");
+    });
+  });
+}
+
+$(document)
+.on('click', '.add-user', function() {
   addUser();
+  listUsers();
+})
+.on('click', '.remove-row', function() {
+  var userId = $(this).data('id');
+  removeUser(userId);
+})
+.on('click', '.edit-row', function() {
+  var userId = $(this).data('id');
+  editUser(userId);
 });
 
 todo.open(function(err, data) {
@@ -30,12 +96,4 @@ todo.open(function(err, data) {
   } 
 });
 
-todo.query('user', function(res) {
-  res.forEach(function(row) {
-    table.append("<tr>");
-    table.append("<td>" + row.id + "</td>");
-    table.append("<td>" + row.name + "</td>");
-    table.append("<td>" + row.age + "</td>");
-    table.append("</tr>");
-  });
-});
+listUsers();
